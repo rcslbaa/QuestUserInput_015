396dbe50ea579872f7a105bcd8892971ef8ce833
rcslbaa <ifasmarani3125@gmail.com>
2025-10-31 16:10:03 +0700

    sumber data tunggal komponen UI

--- app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
@@ -36,3 +36,8 @@ fun FormDataDiri(modifier: Modifier
     var textNama by remember {mutableStateOf(value="")}
     var textAlamat by remember {mutableStateOf(value="")}
     var textJK by remember {mutableStateOf(value="")}
+
+    //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember {mutableStateOf(value="")}
+    var alamat by remember {mutableStateOf(value="")}
+    var jenis by remember {mutableStateOf(value="")}
