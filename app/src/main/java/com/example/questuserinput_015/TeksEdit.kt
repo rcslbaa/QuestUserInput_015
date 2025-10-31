a1540dbd12b6d3d473e7563cdf8e656fdfda69cd
rcslbaa <ifasmarani3125@gmail.com>
2025-10-31 16:18:02 +0700

    membuat daftar pilihan

--- app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
@@ -57,3 +57,18 @@ fun FormDataDiri(modifier: Modifier
                 textNama = it
             }
         )
+        Row {
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = { textJK = item }
+                ), verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        })
+                    Text(text=item)
+                }
+            }
+        }
