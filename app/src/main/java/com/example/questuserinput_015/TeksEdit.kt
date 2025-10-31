c94034430da7f9b7bd147b410534a6511449a295
rcslbaa <ifasmarani3125@gmail.com>
2025-10-31 16:21:37 +0700

    membuat garis horizontal berwarna abu-abu gelap

--- app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_015/TeksEdit.kt
@@ -82,4 +82,10 @@ fun FormDataDiri(modifier: Modifier
                 textAlamat = it
             }
         )
-
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(id = R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
