ff33bd0dc9c85c4124de470d8198293aebf67e90
rcslbaa <ifasmarani3125@gmail.com>
2025-11-02 15:06:06 +0700

    membuat fungsi UI utama

---
+++ app/src/main/java/com/example/questuserinput_015/Praktikum4.kt
@@ -0,0 +1,27 @@
+package com.example.questuserinput_015
+
+import android.app.DatePickerDialog
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import java.util.*
+
+@Composable
+fun Praktikum(modifier: Modifier = Modifier) {
